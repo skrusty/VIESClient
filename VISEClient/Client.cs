7016c74b11c8d92e0ee545f73428e920e8802124
skrusty <ben@xdev.net>
2016-02-08 19:05:34 +0000

    Initial Commit

---
+++ VISEClient/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using VISEClient.SoapEndpoint;
+
+namespace VISEClient
+{
+    public class Client
+    {
+        public VISEResponse CheckVATNumber(string countryCode, string vatNumber)
+        {
+            var vClient = new checkVatPortTypeClient();
+            var isValid = false;
+            try
+            {
+                string name;
+                string address;
+                vClient.checkVat(ref countryCode, ref vatNumber, out isValid, out name, out address);
+                return new VISEResponse()
+                {
+                    IsValid = isValid,
+                    Name = name,
+                    Address = address,
+                    CountryCode = countryCode,
+                    VATNumber = vatNumber
+                };
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"VISE threw an exception {ex.Message}");
+                return new VISEResponse()
+                {
+                    IsValid = isValid,
+                    CountryCode = countryCode,
+                    VATNumber = vatNumber
+                };
+            }
+        }
+    }
+
+    public class VISEResponse
+    {
+        public bool IsValid { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string CountryCode { get; set; }
+        public string VATNumber { get; set; }
+    }
+}
---
+++ VISEDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VISEClient;
+
+namespace VISEDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Client();
+            Console.Write("Enter Country Code (e.g. GB): ");
+            var countryCode = Console.ReadLine();
+            Console.Write("Enter VAT Number: ");
+            var vatNumber = Console.ReadLine();
+            var result = client.CheckVATNumber(countryCode, vatNumber);
+
+            Console.WriteLine($"Check returned {result.IsValid}");
+            Console.WriteLine($"Company: {result.Name}");
+            Console.WriteLine($"Address: {result.Address}");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
